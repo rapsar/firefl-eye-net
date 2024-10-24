fe8c102c574d9e5c5fb559a7d4dd538566f3cd4b
Raphael <48169917+rapsar@users.noreply.github.com>
2024-10-24 16:38:48 -0400

    Delete code/mat/main_net.m

--- code/mat/main_net.m
+++
@@ -1,9 +0,0 @@
-%% define training set
-fdir = {'',''};
-bdir = {'',''};
-
-%% to train a ffnet with RGB patches
-ffnet_rgb_241025 = ffnetTrainCNNrgb(fdir,bdir);
-
-%% to train a ffnet with grayscale patches
-ffnet_gs_241025 = ffnetTrainCNNgs(fdir,bdir);
