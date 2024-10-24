18164cd027046996df3a56a86ec8c6cd293f4fe3
Raphael <48169917+rapsar@users.noreply.github.com>
2024-10-24 16:32:49 -0400

    Update main_net.m

--- code/mat/main_net.m
+++ code/mat/main_net.m
@@ -1,6 +1,6 @@
 %% define training set
-fdir = {'gp360/ffeye/f-220718AZMSSEcGPM1m02','gp360/ffeye/f-220718AZMSSEcGPM1m03','gp360/ffeye/f-220718AZMSSEcGPM1m05'};
-bdir = {'gp360/ffeye/b-220718AZMSSEcGPM1m02','gp360/ffeye/b-220718AZMSSEcGPM1m03'};
+fdir = {'',''};
+bdir = {'',''};
 
 %% to train a ffnet with RGB patches
 ffnet_rgb_241025 = ffnetTrainCNNrgb(fdir,bdir);
